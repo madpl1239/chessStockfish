e800bc481cb99fc3ab8398cf597bb5f8605280a6
madpl <madmax1239@gmail.com>
2025-01-01 20:04:08 +0100

    testCommand: added defines.hpp, modified main.cpp - added two functions to testing

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,28 @@
  * main.cpp
  * 
  */
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
+
+std::string toChessNote(sf::Vector2f piece)
+{
+	std::string str;
+	
+	str += char((piece.x / TILE_SIZE) + 97);
+	str += char(7 - (piece.y / TILE_SIZE) + 49);
+	
+	return str;
+}
+
+
+sf::Vector2f toCoords(char a, char b)
+{
+	int x = int(a) - 97;
+	int y = 7 - int(b) + 49;
+	
+	return sf::Vector2f(x * TILE_SIZE, y * TILE_SIZE);
+}
 
 
 int main(void)
