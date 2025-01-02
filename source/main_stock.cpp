449aa9c968de71df7c8f2be431a43b25b9ed701b
madpl <madmax1239@gmail.com>
2025-01-02 19:20:13 +0100

    main: modified main.cpp --> added try{} catch{}

--- source/main_stock.cpp
+++ source/main_stock.cpp
@@ -13,47 +13,57 @@
 
 int main(void)
 {
-	sf::RenderWindow window(sf::VideoMode(BOARD_SIZE, BOARD_SIZE), "Chess by madpl - 2024");
-	window.setPosition(sf::Vector2i(500, 200));
-	window.setFramerateLimit(60);
-	
-	Chess chess;
-	chess.setupBoard();
-	
-	Stockfish engine("./stockfish");
-	
-	engine.sendCommand("uci");
-	std::string resp = engine.getResponse();
-	
-	std::cout << "resp.size() = " << resp.size() << "\n";
-	std::cout << "resp = " << resp << "\n";
-	
-	resp.clear();
-	engine.sendCommand("isready");
-	resp = engine.getResponse();
-	
-	std::cout << "resp.size() = " << resp.size() << "\n";
-	std::cout << "resp = " << resp;
-	
-	resp.clear();
-	
-	while(window.isOpen())
+	try
 	{
-		sf::Event event;
-		while(window.pollEvent(event))
+		sf::RenderWindow window(sf::VideoMode(BOARD_SIZE, BOARD_SIZE), "Chess by madpl - 2024");
+		window.setPosition(sf::Vector2i(500, 200));
+		window.setFramerateLimit(60);
+		
+		Chess chess;
+		chess.setupBoard();
+		
+		Stockfish engine("./stockfish");
+		
+		engine.sendCommand("uci");
+		std::string resp = engine.getResponse();
+		
+		std::cout << "resp.size() = " << resp.size() << "\n";
+		std::cout << "resp = " << resp << "\n";
+		
+		resp.clear();
+		engine.sendCommand("isready");
+		resp = engine.getResponse();
+		
+		std::cout << "resp.size() = " << resp.size() << "\n";
+		std::cout << "resp = " << resp;
+		
+		resp.clear();
+		
+		while(window.isOpen())
 		{
-			if(event.type == sf::Event::Closed)
-				window.close();
+			sf::Event event;
+			while(window.pollEvent(event))
+			{
+				if(event.type == sf::Event::Closed)
+					window.close();
+				
+				chess.handleMouseEvent(event, window);
+			}
 			
-			chess.handleMouseEvent(event, window);
+			window.clear();
+			chess.draw(window);
+			window.display();
 		}
 		
-		window.clear();
-		chess.draw(window);
-		window.display();
+		std::cout << "\ndone.\n";
 	}
 	
-	std::cout << "\ndone.\n";
+	catch(const std::exception &e)
+	{
+		std::cerr << "error: " << e.what() << "\n";
+		
+		return -1;
+	}
 	
 	return 0;
 }
